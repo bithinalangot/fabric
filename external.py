a309f68ac25be98d5d9534ac17d58532bd257611
Bithin Alangot <bithin2007@gmail.com>
2019-12-25 10:11:54 +0800

    added binary to external.py

--- external.py
+++ external.py
@@ -1,4 +1,11 @@
 import sys
-print("Verification: ", sys.argv[1])
-print("Proof: ", sys.argv[2])
-print("Public Input:", sys.argv[3])
+import subprocess
+import re
+
+result = subprocess.check_output(["./verifier", sys.argv[1], sys.argv[2], sys.argv[3]])
+verifier = re.search("true", str(result))
+
+if verifier:
+    print 1
+else:
+    print 0
